26dd81cf7592d975e861724d815b463619329937
leke-osez <leke.osez@gmail.com>
2024-05-22 11:10:16 +0100

    FIXED case error on select-resume-page

--- src/app/(resumes)/select-resume-template/page.tsx
+++ src/app/(resumes)/select-resume-template/page.tsx
@@ -1,36 +1,12 @@
 "use client";
 
-import RoundedButton from "@/components/buttons/RoundedButton";
-import InputField from "@/components/inputs/InputField";
-import FilterResume, {
-  initialResumeQueryData,
-} from "@/containers/resume/selectResume/FilterResume";
-import ResumeTemplate from "@/containers/resume/selectResume/ResumeTemplateLists";
-import { ResumeFilterQueryType } from "@/shared/types/resumeTypes";
-import React, { useState } from "react";
+import SelectResumePage from "@/pages/SelectResumePage";
 
 const page = () => {
-  const [resumeFilterQuery, setResumeFilterQuery] = useState(
-    initialResumeQueryData
-  );
-  const handleRemoveTag = (id: string) => {};
-  const applyResumeFilterQuery = (filterQuery: ResumeFilterQueryType) => {};
+
 
   return (
-    <div
-      className={` dark:bg-primary_dark flex flex-row gap-4 flex-1 h-full relative py-6`}
-    >
-      <div className="sticky top-24 h-fit w-fit">
-        <FilterResume applyResumeFilterQuery={applyResumeFilterQuery} />
-      </div>
-      <ResumeTemplate
-        handleRemoveTag={handleRemoveTag}
-        resumeFilterQuery={resumeFilterQuery}
-        resumeList={[]}
-      />
-
-      {/* </main> */}
-    </div>
+    <SelectResumePage/>
   );
 };
 
---
+++ src/pages/SelectResumePage.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import FilterResume, {
+  initialResumeQueryData,
+} from "@/containers/resume/selectResume/FilterResume";
+import ResumeTemplateLists from "@/containers/resume/selectResume/ResumeTemplateLists";
+import { ResumeFilterQueryType } from "@/shared/types/resumeTypes";
+import React, { useState } from "react";
+
+const SelectResumePage = () => {
+  const [resumeFilterQuery, setResumeFilterQuery] = useState(
+    initialResumeQueryData
+  );
+  const handleRemoveTag = (id: string) => {};
+  const applyResumeFilterQuery = (filterQuery: ResumeFilterQueryType) => {};
+
+  return (
+    <div
+      className={` dark:bg-primary_dark flex flex-row gap-4 flex-1 h-full relative py-6`}
+    >
+      <div className="sticky top-24 h-fit w-fit">
+        <FilterResume applyResumeFilterQuery={applyResumeFilterQuery} />
+      </div>
+      <ResumeTemplateLists
+        handleRemoveTag={handleRemoveTag}
+        resumeFilterQuery={resumeFilterQuery}
+        resumeList={[]}
+      />
+
+      {/* </main> */}
+    </div>
+  );
+};
+
+export default SelectResumePage;
